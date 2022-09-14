76ae69a6f9e8890c729ac3ae4349bc1e0461eae5
dongjun0128 <dongjun9701@naver.com>
2022-09-14 14:08:37 +0900

    Update 문제1.cpp

--- 2주차 - 우선순위 큐/문제1.cpp
+++ 2주차 - 우선순위 큐/문제1.cpp
@@ -18,13 +18,36 @@ int main()
     //할당에 오류가 생기면 강제종료
     if (arr == NULL) return-1;
 
-    for (int arr_num = 0; arr_num < size; arr_num++ ) {
+    //입력값 저장
+    for (int arr_num = 0; arr_num < size; arr_num++) {
         scanf("%d", &arr[arr_num]);
         getchar();
     }
 
+    //제자리 선택 정렬
+    for (int arr_num1 = 0; arr_num1 < size; arr_num1++) {
+        int min = arr[arr_num1];
+        int key = -1;
+
+        // arr[arr_num1]보다 작은값이 있는지 확인
+        for (int arr_num2 = arr_num1 + 1; arr_num2 < size; arr_num2++) {
+            if (min > arr[arr_num2]) {
+                min = arr[arr_num2];
+                key = arr_num2;
+            }
+        }
+
+        // arr[arr_num1] 보다 작은값이 존재하면 자리 바꾸기
+        if (key != -1) {
+            int temp = arr[arr_num1];
+            arr[arr_num1] = arr[key];
+            arr[key] = temp;
+        }
+    }
+
+
     for (int arr_num = 0; arr_num < size; arr_num++) {
-        printf("%d", arr[arr_num]);
+        printf(" %d", arr[arr_num]);
     }
 
     //동적할당 해제
