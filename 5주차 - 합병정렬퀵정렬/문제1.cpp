e2baadf3e5c9f8fb32586f8b976d2e34c7196a00
dongjun0128 <dongjun9701@naver.com>
2022-10-17 17:15:12 +0900

    add problem

---
+++ 5주차 - 합병정렬퀵정렬/문제1.cpp
@@ -0,0 +1,60 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+
+int M[100] = { 0 };
+
+void mergeSort(int* p, int l, int r);
+void merge(int* p, int l, int m, int r);
+
+int main() {
+	int n, i;
+	scanf("%d", &n);
+	//동적 할당
+	int* p = (int*)malloc(sizeof(int) * n);
+	for (i = 0;i < n;i++) {
+		scanf("%d", &p[i]);
+	}
+	mergeSort(p, 0, n - 1);
+	for (i = 0;i < n;i++) {
+		printf(" %d", p[i]);
+	}
+
+	//해제
+	free(p);
+}
+
+//합병
+void merge(int* p, int l, int m, int r) {
+	int i = l, k = l;
+	int j = m + 1;
+	while (i <= m && j <= r) {
+		if (p[i] <= p[j]) {
+			M[k++] = p[i++];
+		}
+		else {
+			M[k++] = p[j++];
+		}
+	}
+	while (i <= m) {
+		M[k++] = p[i++];
+	}
+	while (j <= r) {
+		M[k++] = p[j++];
+	}
+	for (k = l;k <= r;k++) {
+		p[k] = M[k];
+	}
+}
+
+//합병 정렬
+void mergeSort(int* p, int l, int r) {
+	if (l < r) {
+		int m = (l + r) / 2;
+		mergeSort(p, l, m);
+		mergeSort(p, m + 1, r);
+		merge(p, l, m, r);
+	}
+}
+
---
+++ 5주차 - 합병정렬퀵정렬/문제2.cpp
@@ -0,0 +1,48 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<windows.h>
+
+//제자리 퀵 정렬
+void inPlaceQuickSort(int* p, int l, int r) {
+	if (l >= r) {
+		return;
+	}
+	int pivot = r;
+	int i = l;
+	int j = r - 1;
+	int tmp;
+	while (i <= j) {
+		while (i <= j && p[i] <= p[pivot]) { i++; }
+		while (j >= i && p[j] >= p[pivot]) { j--; }
+		if (i > j) {
+			tmp = p[pivot];
+			p[pivot] = p[i];
+			p[i] = tmp;
+		}
+		else {
+			tmp = p[i];
+			p[i] = p[j];
+			p[j] = tmp;
+		}
+	}
+	inPlaceQuickSort(p, l, i - 1);
+	inPlaceQuickSort(p, i + 1, r);
+}
+
+int main() {
+	int n, i;
+	scanf("%d", &n);
+	//동적할당
+	int* p = (int*)malloc(sizeof(int) * n);
+	for (i = 0;i < n;i++) {
+		scanf("%d", &p[i]);
+	}
+	inPlaceQuickSort(p, 0, n - 1);
+	for (i = 0;i < n;i++) {
+		printf(" %d", p[i]);
+	}
+
+	//해제
+	free(p);
+}
