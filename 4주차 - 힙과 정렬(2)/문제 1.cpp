3ce91e47b10aec087bfd9e65622d318232a9e191
dongjun0128 <dongjun9701@naver.com>
2022-10-17 16:05:31 +0900

    add problem

---
+++ 4주차 - 힙과 정렬(2)/문제 1.cpp
@@ -0,0 +1,81 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+int H[99] = { 0 };
+int heap_sort[99] = { 0 };
+int n = 0;
+int size = 0;
+
+// 자식 노드보다 내가 더 작으면 내려가는 함수
+// Left_Index = Index * 2 
+// Right_Index = Index * 2 + 1
+void downheap(int i) {
+	int tmp;
+
+	if (H[2 * i] > H[2 * i + 1]) {
+		if (H[2 * i] > H[i]) {
+			tmp = H[i * 2];
+			H[i * 2] = H[i];
+			H[i] = tmp;
+			downheap(i * 2);
+		}
+	}
+	else {
+		if (H[2 * i + 1] > H[i]) {
+			tmp = H[i * 2 + 1];
+			H[i * 2 + 1] = H[i];
+			H[i] = tmp;
+			downheap(i * 2 + 1);
+		}
+	}
+}
+
+//힙 설계
+void rbuild(int i) {
+	if (i > n) {
+		return;
+	}
+	rbuild(2 * i);
+	rbuild(2 * i + 1);
+	downheap(i);
+	return;
+}
+
+//제자리 힙 정렬
+void inPlaceHeapSort() {
+	int i;
+	for (i = size;i > 1;i--) {
+		int tmp;
+		tmp = H[1];
+		H[1] = H[i];
+		H[i] = 0;
+		heap_sort[i] = tmp;
+		n--;
+		downheap(1);
+	}
+	heap_sort[1] = H[1];
+}
+
+//힙 출력
+void printArray() {
+	int i;
+	for (i = 1;i <= size;i++) {
+		printf(" %d", heap_sort[i]);
+	}
+}
+
+
+int main() {
+	int k, i;
+	scanf("%d", &k);
+	for (i = 1;i <= k;i++) {
+		scanf("%d", &H[i]);
+		n++;
+		size++;
+	}
+	rbuild(1);
+	inPlaceHeapSort();
+	printArray();
+}
---
+++ 4주차 - 힙과 정렬(2)/문제 2.cpp
@@ -0,0 +1,74 @@
+﻿#include<stdio.h>
+int heap[99] = { 0 };
+int heap_sort[99] = { 0 };
+int n = 0;
+int size = 0;
+
+// 자식 노드보다 내가 더 작으면 내려가는 함수
+// Left_Index = Index * 2 
+// Right_Index = Index * 2 + 1
+void downheap(int i) {
+	int tmp;
+	if (heap[2 * i] > heap[2 * i + 1]) {
+		if (heap[2 * i] > heap[i]) {
+			tmp = heap[i * 2];
+			heap[i * 2] = heap[i];
+			heap[i] = tmp;
+			downheap(i * 2);
+		}
+	}
+	else {
+		if (heap[2 * i + 1] > heap[i]) {
+			tmp = heap[i * 2 + 1];
+			heap[i * 2 + 1] = heap[i];
+			heap[i] = tmp;
+			downheap(i * 2 + 1);
+		}
+	}
+}
+
+//힙 설계
+void rbuild(int i) {
+	if (i > n) {
+		return;
+	}
+	rbuild(2 * i);
+	rbuild(2 * i + 1);
+	downheap(i);
+	return;
+}
+
+//힙 출력
+void printArray() {
+	int i;
+	for (i = 1;i <= size;i++) {
+		printf(" %d", heap_sort[i]);
+	}
+}
+
+//힙 정렬
+void inPlaceHeapSort() {
+	int i;
+	for (i = size;i > 1;i--) {
+		int tmp;
+		tmp = heap[1];
+		heap[1] = heap[i];
+		heap[i] = 0;
+		heap_sort[i] = tmp;
+		n--;
+		downheap(1);
+	}
+	heap_sort[1] = heap[1];
+}
+int main() {
+	int k, i;
+	scanf("%d", &k);
+	for (i = 1;i <= k;i++) {
+		scanf("%d", &heap[i]);
+		n++;
+		size++;
+	}
+	rbuild(1);
+	inPlaceHeapSort();
+	printArray();
+}
